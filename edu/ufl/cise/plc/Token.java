381b5a2971da221b67adf47dfd7f2751b4eb29aa
raghuweer23 <42888633+raghuweer23@users.noreply.github.com>
2022-01-28 10:18:04 -0500

    nothing major

--- edu/ufl/cise/plc/Token.java
+++ edu/ufl/cise/plc/Token.java
@@ -1,11 +1,7 @@
 package edu.ufl.cise.plc;
 
 import java.util.HashMap;
-
-import edu.ufl.cise.plc.IToken.Kind;
-import edu.ufl.cise.plc.IToken.SourceLocation;
-
-import java.util.*;
+import java.util.Scanner;
 
 public class Token implements IToken {
 
@@ -13,15 +9,15 @@ public class Token implements IToken {
     private String input;
 
     int lexemmelength;
-    static int state;
-    static int done;
-    static int finalState;
+    boolean done;
     int pos = 0;
 
     public enum State {
         START, IN_IDENT, HAVE_ZERO, HAVE_DOT,
-        IN_FLOAT, IN_NUM, HAVE_EQ, HAVE_MINUS;
+        IN_FLOAT, IN_NUM, HAVE_EQ, HAVE_MINUS, HAVE_PLUS;
     }
+
+    State state = State.START;
 
     @Override
     public Kind getKind() {
@@ -35,78 +31,77 @@ public class Token implements IToken {
 
     @Override
     public SourceLocation getSourceLocation() {
-
+        return null;
     }
 
-    public Lexer(){
+    public void Lexer() {
         int tok;
 
-        done = 0;
-        state = 0;
-        finalState = -1;
+        done = false;
 
-        double x = 0.123;
-        
-
-        while(done != 0){
-            step();
-            setFinal();
-        }
-        if(finalState<0){
-
-        }
     }
 
-    HashMap<Kind, string> rsrvd = new HashMap<>(); // hashmap for the reserved words of the programming language, we
-                                                   // will use this after we have the identifier and we will compare it
-                                                   // with the hashmap.
+    HashMap<Kind, State> rsrvd = new HashMap<>(); // hashmap for the reserved words of the programming language, we
+                                                  // will use this after we have the identifier and we will compare it
+                                                  // with the hashmap.
 
     // rsrvd.put(TYPE, 'int'), somehow create a hashmap of all the reserved words
     // like this
-
-    public void moveToState(State st) {
-        state = st;
-    }
 
     Scanner s = new Scanner(System.in);
     char chars[] = s.next().toCharArray();
 
     public void step(){
+        while(done!=true){
         char ch = chars[pos]; //Can i find some other way to iterate over the complete code, for loop ? - too tacky plus conditions will make it difficult
                            //while loop? 
+        int             
+        
         switch(state){ //implementing the DFA, this switch statement is to define the states of the DFA
             case START:
             int startPOS = pos;
                 switch(ch){ //this switch case works on the characters of the string.
-                    case 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-                    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', '$', '_':
+                    case 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z':
+                    case 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z':
+                    case '$':
+                    case '_':
+                    {
+                        pos++;
+                        state = State.IN_IDENT;
+                        kind = Kind.PLUS;
+                        //array list TOKENS<String "identfier",string "p">
+                    }
+                    //to do : Move to some different state 
+                    //state = have 
                     //return identifier or save it somewhere to let the program know, save it in array list ? umm how about 
                     //we save it like ('string','token') but this will have to be done in the end of the execution of the dfa
                     //let's do it like this, create an array list(string name, token kind)
                     //where as soon as we hit the final state we add it into the array list. and array list can be named tokens? 
-                    moveToState(IN_IDENT,ch);
                     break;
                     default:
-                    done =1;
-
                     case '+':
-                        kind = Kind.PLUS;
-                        pos++
-
+                    {
+                       kind = Kind.PLUS;
+                        pos++;
+                        state = State.HAVE_PLUS;
+                    }
                 }
-            case State.IN_IDENT:
+            case IN_IDENT:
                 switch(ch){
                 case 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y', 'z':
+                case 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z':
+                case '$':
+                case '_':
                 case '1','2','3','4','5','6','7','8','9':
                 {
 
                 }
             }
-            case State.HAVE_ZERO:
+            case HAVE_ZERO:
                 {
                 
                 }
-            case State.HAVE_DOT:
+            case HAVE_DOT:
             {
 
             }
@@ -116,9 +111,10 @@ public class Token implements IToken {
             }
 
             default:
-            throw new 
+            throw new IllegalStateException("lexer bug");   
                 
         }
+    }
     }
 
     @Override
