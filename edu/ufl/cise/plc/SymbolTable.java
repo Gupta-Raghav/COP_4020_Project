cf020507c52de2f752c3654f5d2899f5993857a7
raghuweer23 <42888633+Gupta-Raghav@users.noreply.github.com>
2022-04-27 17:19:05 -0400

    added new funcitons in symbol table

--- edu/ufl/cise/plc/SymbolTable.java
+++ edu/ufl/cise/plc/SymbolTable.java
@@ -6,13 +6,20 @@ import java.util.HashMap;
 public class SymbolTable {
     // TODO: Implement a symbol table class that is appropriate for this language.
     HashMap<String, Declaration> entries = new HashMap<>();
-
+    String ProgramName;
     public boolean insert(String name, Declaration declaration) {
         return (entries.putIfAbsent(name, declaration) == null);
     }
 
     public Declaration lookup(String name) {
         return entries.get(name);
+    }
+    public boolean cont(String name) {
+        return entries.containsKey(name);
+    }
+
+    public void setName(String name) {
+        programName = name;
     }
 
     public void delete(String name) {
